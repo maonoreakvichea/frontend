16a1730e5ded676fe2daa2887a5d35cd49d755f6
Your Name <maonoreakvichea@gmail.com>
2026-02-12 12:52:58 +0700

    feat : w5

---
+++ W4/exercise2/src/components/Footer.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Footer(){
+    return (
+      <footer class="block">
+      <p>You can find the React doc at https://react.dev/</p>
+    </footer>
+    )
+}
---
+++ W4/exercise2/src/components/Header.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Header(){
+    return (
+      <header className="block">
+        <h2>WELCOME !</h2>
+      </header>
+    )
+}
